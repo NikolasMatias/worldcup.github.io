b3cb7122f817c03ca6523faf0d873e7f419113d6
Nikolas Matias <nikolas.matias500@gmail.com>
2018-07-09 11:27:56 -0300

    Created view per group of teams.

--- assets/js/controllers/worldcup.controller.js
+++ assets/js/controllers/worldcup.controller.js
@@ -1,28 +1,33 @@
-const Worldcup = {
-    install(Vue, options) {
-        Vue.mixin({
-            data: function() {
-                return {
-                    urlWorldCup: 'https://worldcup.sfg.io',
-                    group_teams: []
-                };
-            },
-            watch: {
+let WorldCup = {
+    data: function() {
+        return {
+            urlWorldCup: 'https://worldcup.sfg.io',
+            group_teams: [],
+            team_maches: [],
+            fifa_code: ''
+        };
+    },
+    watch: {
 
-            },
-            methods: {
-                getGroupResults: function () {
-                    let self = this;
+    },
+    methods: {
+        getGroupResults: function () {
+            let self = this;
 
-                    axios.get(this.urlWorldCup).then(function (result) {
-                        self.group_teams = result;
-                    }).catch(function (errors) {
-                        console.log(errors);
-                    });
-                }
-            }
-        });
+            axios.get(this.urlWorldCup+"/teams/group_results").then(function (result) {
+                self.group_teams = result.data;
+            }).catch(function (errors) {
+                console.log(errors);
+            });
+        },
+        getMatchesCountry: function (fifa_code) {
+            let self = this;
+
+            axios.get(this.urlWorldCup+"/matches/country?fifa_code="+fifa_code).then(function (result) {
+                self.team_maches = result.data;
+            }).catch(function (errors) {
+                console.log(errors);
+            });
+        }
     }
 };
-
-export default Worldcup;
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
-import Worldcup from "./controllers/worldcup.controller";
-
 Vue.component('v-select', VueSelect.VueSelect);
 
-Vue.use(Worldcup);
 
 new Vue({
     el: '#worldcup',
+    mixins: [WorldCup],
     data: {
 
     },
